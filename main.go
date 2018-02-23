046295df2557862e5dbb81cc4c6d49f069469bb7
Jarred Parr <jparr721@gmail.com>
2018-02-23 17:06:22 -0500

    Boilerplate.

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+const (
+	databaseHost     = "localhost"
+	databasPort      = 5432
+	dataBaseUser     = "receipts"
+	databasePassword = "receipts"
+	databaseName     = "stor"
+)
+
+func main() {
+
+}
